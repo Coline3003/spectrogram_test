3c9ca964b16fc2d94fea65c526f570de28313537
Coline3003 <116337158+Coline3003@users.noreply.github.com>
2024-07-17 14:18:25 +0200

    Update tt_um_Coline3003_top.v

--- src/tt_um_Coline3003_top.v
+++ src/tt_um_Coline3003_top.v
@@ -31,7 +31,7 @@ module tt_um_Coline3003_top(
 	     .sending_data(uo_out[7]));
 	
 	assign uo_out[1] = 0;
-	assign uio_in[6:0] = 7'b0;
+	
 	assign uio_out[7] = 0;
     assign uio_oe = 8'b11111110; //inputs enable
 endmodule // top
