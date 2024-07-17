84773a0c7a8812fb5bafde1f7185a0fc887fd5fb
Coline3003 <116337158+Coline3003@users.noreply.github.com>
2024-07-17 13:56:02 +0200

    Update top.v

--- src/top.v
+++ src/top.v
@@ -1,15 +1,17 @@
 module top(
-    	input input_acquisition_clk, input_serial_readout_clk, RTC_clk, 	reset,
-	 input wire [6:0] ch1, ch2, 
-	 output wire [1:0] serial_out,	 
-	 output SL_ch, SL_time, serial_readout, signal_detected, memorization_completed,sending_data  
+    	input input_acquisition_clk, input_serial_readout_clk, RTC_clk, reset,
+	 input wire [6:0] ch1,  
+	 output wire serial_out,	 
+	 output SL_ch, SL_time, serial_readout, signal_detected, memorization_completed,sending_data, read_bank,
+	write_bank, sending_started, sending_pending,
+	output [2:0] state_reg_FSM
 
 );
 
 	wire [8:0] addr_in;
 	wire [8:0] addr_out;
-	wire [2:0] data_decode [1:0];
-	wire [2:0] data_sent [1:0];
+	wire [2:0] data_decode;
+	wire [2:0] data_sent;
 	//wire signal_detected;
 	wire acquisition_clk_enable;
 	wire serial_readout_clk_enable;
@@ -21,39 +23,37 @@ module top(
 
 
 wire [7:0] idx_final;
-
+	assign read_bank = addr_out[8];
+	assign write_bank = addr_in[8];
 
 memory_controller mem_ctl(.clk(acquisition_clk), .reset(reset), .signal_detected(signal_detected), .addr_in(addr_in[8:0]), .idx_final(idx_final[7:0]), .we(we), .bank0_full(bank0_full), .bank1_full(bank1_full),
 				.memorization_completed(memorization_completed), .state_reg(state_reg_memctr[1:0]));
 
-FSM fsm(.clk(serial_readout_clk),.reset(reset), .bank0_full(bank0_full), .bank1_full(bank1_full), .memorization_completed(memorization_completed), .idx_final(idx_final[7:0]),
-		.addr_out(addr_out[8:0]), .SL_ch(SL_ch), .SL_time(SL_time), .selection_bit(selection_bit), 
-		.re(re), .serial_readout(serial_readout), .sending_data(sending_data));
+FSM fsm(.clk(serial_readout_clk),.reset(reset), .bank0_full(bank0_full), .bank1_full(bank1_full), .memorization_completed(memorization_completed), 
+	.idx_final(idx_final[7:0]), .addr_out(addr_out[8:0]), .SL_ch(SL_ch), .SL_time(SL_time), .selection_bit(selection_bit), 
+	.re(re), .serial_readout(serial_readout), .sending_data(sending_data), .state_reg(state_reg_FSM), .sending_started(sending_started), 
+	.sending_pending(sending_pending));
 
 		
 	//signal detector
-	assign signal_detected = ch1[0] || ch2[0];
+	assign signal_detected = ch1[0];
 	
 	//clk _activators
 	assign acquisition_clk_enable = memorization_completed || signal_detected || we || (state_reg_memctr != 0) ;
-	assign serial_readout_clk_enable = memorization_completed || serial_readout || acquisition_clk_enable ;
+	assign serial_readout_clk_enable = memorization_completed || serial_readout || acquisition_clk_enable || (state_reg_FSM != 0) ;
 	assign acquisition_clk = acquisition_clk_enable && input_acquisition_clk; 
 	assign serial_readout_clk = serial_readout_clk_enable && input_serial_readout_clk;
 	
 	
 	//encode data
-	encoder enc1( .channel(ch1[6:0]), .channel_decode(data_decode[0][2:0]));
-	encoder enc2( .channel(ch2[6:0]), .channel_decode(data_decode[1][2:0]));
+	encoder enc1( .channel(ch1[6:0]), .channel_decode(data_decode[2:0]));
 
+	memory memory1(.clk_write(acquisition_clk),.clk_read(serial_readout_clk),.write_enable(we), .address_in(addr_in[8:0]), .data_in(data_decode[2:0]),
+		       .read_enable(re), .address_out(addr_out[8:0]), .data_out(data_sent[2:0]));
+  	
+	PISO_register reg1(.clk(serial_readout_clk), .reset(reset), .SL(SL_ch), .parallel_in(data_sent[2:0]), .serial_out(PISO_ch1));
 	
-	memory memory1(.clk_write(acquisition_clk),.clk_read(serial_readout_clk),.write_enable(we), .address_in(addr_in[8:0]), .data_in(data_decode[0][2:0]), .read_enable(re), .address_out(addr_out[8:0]), .data_out(data_sent[0][2:0]));
-  	memory memory2(.clk_write(acquisition_clk),.clk_read(serial_readout_clk),.write_enable(we), .address_in(addr_in[8:0]), .data_in(data_decode[1][2:0]), .read_enable(re), .address_out(addr_out[8:0]), .data_out(data_sent[1][2:0]));
-
-	PISO_register reg1(.clk(serial_readout_clk), .reset(reset), .SL(SL_ch), .parallel_in(data_sent[0][2:0]), .serial_out(PISO_ch1));
-	PISO_register reg2(.clk(serial_readout_clk),.reset(reset), .SL(SL_ch), .parallel_in(data_sent[1][2:0]), .serial_out(serial_out[1]));
-
-
-	mux_2to1 mux(.PISO_ch1(PISO_ch1), .PISO_time(PISO_time), .select(selection_bit), .data_out(serial_out[0]));
+	mux_2to1 mux(.PISO_ch1(PISO_ch1), .PISO_time(PISO_time), .select(selection_bit), .data_out(serial_out));
 
 	wire [31:0] event_time;
 	wire [31:0] event_time_out;
